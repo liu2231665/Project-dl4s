79420cdca0890de760dc1ce37233ca420bb94740
Yingru Liu <liu2231665@hotmail.com>
2017-11-08 08:19:05 -0500

    add experiments.

--- Projects/AudioEffects/audioRNN_II.py
+++ Projects/AudioEffects/audioRNN_II.py
@@ -6,7 +6,7 @@ Descriptions: the code to train and run the gaussRNN from dl4s.autoregRNN
               under the "Blizzard 2013 data-set".
               ----2017.11.03
 #########################################################################"""
-from dl4s import gaussRNN_cov, configRNN
+from dl4s import gaussRNN, configRNN
 from Projects.AudioEffects.fetchData import fetchData
 from Projects.AudioEffects.rmseTool import rmseRNN
 import os
@@ -15,7 +15,7 @@ configRNN.unitType = "GRU"
 configRNN.Opt = 'Adam'
 configRNN.savePath = "./audioRNN-II/"
 configRNN.eventPath = "./audioRNN-II/"
-configRNN.dimLayer = [150, 500, 150]
+configRNN.dimLayer = [150, 500, 500, 150]
 configRNN.init_scale = 0.01
 SAVETO = './audioRNN-II/historyRNN.npz'
 
@@ -33,13 +33,13 @@ if __name__ == '__main__':
             os.makedirs(configRNN.savePath)
         # Add the save file name into the save path.
         configRNN.savePath = os.path.join(configRNN.savePath, 'RNN-II')
-        RNN = gaussRNN_cov(configRNN)
-        RNN.full_train(dataset=Dataset, maxEpoch=300, batchSize= 75, earlyStop=10, learning_rate=0.0001,
-                       valid_batchSize=75, saveto=SAVETO)
+        RNN = gaussRNN(configRNN)
+        RNN.full_train(dataset=Dataset, maxEpoch=300, batchSize= 125, earlyStop=10, learning_rate=0.001,
+                       valid_batchSize=125, saveto=SAVETO)
 
     if Flag == 'evaluation':
         configRNN.loadPath = os.path.join(configRNN.savePath, 'RNN-I')
-        RNN = gaussRNN_cov(configRNN)
+        RNN = gaussRNN(configRNN)
         print('Evaluation: start computing the RMSE metric.')
         RMSE = rmseRNN(RNN, Dataset['test'], batchSize=125)
         print('The testing reconstructed error is \x1b[1;91m%10.4f\x1b[0m.' % RMSE)
